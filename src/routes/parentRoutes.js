f00cffb4f4a2e5c8bcfc11707e626e096937739b
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-02 22:17:13 +0200

    Update parent route

--- src/routes/parentRoutes.js
+++ src/routes/parentRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { addController, listParents } = require("../controllers/parentController");
+const { addController, getController } = require("../controllers/parentController");
 const router = express.Router();
 
 router.post('/add', addController);
 
 router.get("/all", (req, res) => {
-    listParents(req, res);
+    getController(req, res);
 });
 
 module.exports = router;
