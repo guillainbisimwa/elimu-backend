6b4020d5b14926756c1dad1f023cd62bd167bca1
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-02 22:13:07 +0200

    List annee scolaire

--- src/controllers/anneeScolaireController.js
+++ src/controllers/anneeScolaireController.js
@@ -18,5 +18,23 @@ module.exports = {
     } catch (err) {
       res.status(500).json({ success: false, msg:err.message });
     }
-  }
+  },
+
+  getController: async (req, res) => {
+    try {
+      const anneeScolaire = await db.AnneeScolaire.find();
+      if (!anneeScolaire)
+        return res
+          .status(403)
+          .json({ success: false, msg: "Annee Scolaire doesn't exist" });
+
+      res.status(200).json({
+        success: true,
+        msg: "Annee Scolaire fetched successfully!",
+        anneeScolaire: anneeScolaire,
+      });
+    } catch (err) {
+      res.status(500).json({ success: false, msg: err.message });
+    }
+  },
 }
