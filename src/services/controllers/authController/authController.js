bf98831bc2ceae4d0690cecc9f42176ba0c5052c
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-02 21:59:03 +0200

    Remove registration

--- src/services/controllers/authController/authController.js
+++ src/services/controllers/authController/authController.js
@@ -2,50 +2,7 @@ const jwt = require('jsonwebtoken');
 const db  = require('../../../models');
 
 module.exports = {
-  // receive fullName, email and password
-  registerController : async (req, res)=>{
-    try {
-      var parent = null;
-      const checkAccount = await db.Account.findOne({email: req.body.email});
-
-      if(checkAccount) return res.status(409).json({success:false, msg:"Account Already exist"});
-      const d = { email: req.body.email, password: req.body.password, role: req.headers.role}
-      const account = await db.Account.create(d);
-
-      if (req.headers.role === 'parent') {
-        parent = await db.Parent.create(
-          {
-            fullName: req.body.fullName,
-            phone: req.body.phone,
-            postCode : req.body.postCode,
-            address : req.body.address,
-          });
-
-        if (parent) {
-          account.roleData = { parent: parent._id };
-          await Promise.all([parent.save(), account.save()]);
-        } else {
-          return res.status(500).json({ success: false, msg: 'Internal server error' });
-        }
-      } else {
-        return res.status(500).json({ success: false, msg: 'Internal server error' });
-      }
-      
-      const userTokenObj = {_id: account._id};
-      const token = generateAccessToken(userTokenObj);
-
-      const userCopy = JSON.parse(JSON.stringify(account));
-      const customerCopy = JSON.parse(JSON.stringify(parent)); 
-      delete userCopy.password;
-
-      res.status(200).json({success:true, token: token, account: {...userCopy, ...customerCopy}});
-
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ success: false, msg:err.message });
-    }
-  },
-
+ 
   loginController : async (req, res) =>{
     try{
       const {email, password} = req.body;
