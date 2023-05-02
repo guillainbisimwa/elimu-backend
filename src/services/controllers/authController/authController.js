3b33c9808049a955cb0389570464867b9d6bdd31
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-02 21:57:23 +0200

    Fix issues

--- src/services/controllers/authController/authController.js
+++ src/services/controllers/authController/authController.js
@@ -5,15 +5,15 @@ module.exports = {
   // receive fullName, email and password
   registerController : async (req, res)=>{
     try {
-      var customer = null;
+      var parent = null;
       const checkAccount = await db.Account.findOne({email: req.body.email});
 
       if(checkAccount) return res.status(409).json({success:false, msg:"Account Already exist"});
       const d = { email: req.body.email, password: req.body.password, role: req.headers.role}
       const account = await db.Account.create(d);
 
-      if (req.headers.role === 'customer') {
-        customer = await db.Customer.create(
+      if (req.headers.role === 'parent') {
+        parent = await db.Parent.create(
           {
             fullName: req.body.fullName,
             phone: req.body.phone,
@@ -21,9 +21,9 @@ module.exports = {
             address : req.body.address,
           });
 
-        if (customer) {
-          account.roleData = { customer: customer._id };
-          await Promise.all([customer.save(), account.save()]);
+        if (parent) {
+          account.roleData = { parent: parent._id };
+          await Promise.all([parent.save(), account.save()]);
         } else {
           return res.status(500).json({ success: false, msg: 'Internal server error' });
         }
@@ -35,7 +35,7 @@ module.exports = {
       const token = generateAccessToken(userTokenObj);
 
       const userCopy = JSON.parse(JSON.stringify(account));
-      const customerCopy = JSON.parse(JSON.stringify(customer)); 
+      const customerCopy = JSON.parse(JSON.stringify(parent)); 
       delete userCopy.password;
 
       res.status(200).json({success:true, token: token, account: {...userCopy, ...customerCopy}});
@@ -59,29 +59,17 @@ module.exports = {
 
       const userCopy = JSON.parse(JSON.stringify(account));
       delete userCopy.password;
-      var customerInfo = null;
-      if (account.role === 'customer') {
-        const customer = await db.Customer.findById(account.roleData['customer']);
-        customerInfo = JSON.parse(JSON.stringify(customer)); 
+      var parentInfo = null;
+      if (account.role === 'parent') {
+        const parent = await db.Parent.findById(account.roleData['parent']);
+        parentInfo = JSON.parse(JSON.stringify(parent)); 
       }
-      res.status(200).json({success:true, token: token, account: {...userCopy, ...customerInfo}});
+      res.status(200).json({success:true, token: token, account: {...userCopy, ...parentInfo}});
 
     } catch(err) {
       res.status(500).json({ success: false, msg:err.message })
     } 
   },
-
-  checkEmailController : async (req, res) =>{
-    try{
-      const {email} = req.body;
-      const account = await db.Account.findOne({email: email}).select('+password');
-      if(!account) return res.status(403).json({success:false, msg:"Invalid Email"});
-      res.status(200).json({success:true, msg: "Email exists"});
-
-    } catch(err) {
-      res.status(500).json({ success: false, msg:err.message })
-    } 
-  }
 }
 
 const generateAccessToken = (account) => {
