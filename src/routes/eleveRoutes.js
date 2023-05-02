02efc5052b46f4ff6f667b90351c163747567769
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-02 22:38:36 +0200

    Update routes Eleve

--- src/routes/eleveRoutes.js
+++ src/routes/eleveRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { addController, listEleve } = require("../controllers/eleveController");
+const { addController, getController } = require("../controllers/eleveController");
 const router = express.Router();
 
 router.post('/add', addController);
 
 router.get("/all", (req, res) => {
-    listEleve(req, res);
+    getController(req, res);
 });
 
 module.exports = router;
